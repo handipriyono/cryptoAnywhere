71a615ce955031c507922fe5a85f85b92fdf400d
Handi Priyono <handimacbook@gmail.com>
2024-02-28 09:44:59 +0700

    feat: add init appjs

---
+++ App.tsx
@@ -0,0 +1,7 @@
+import "react-native-gesture-handler";
+import React from "react";
+import DetailCrypto from "./src/screens/DetailCrypto";
+
+export default function App() {
+  return <DetailCrypto />;
+}
