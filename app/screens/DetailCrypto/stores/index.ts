009737eccd5a815ee2ae2d9dae945ad78eece64a
Handi Priyono <handimacbook@gmail.com>
2024-02-28 14:25:39 +0700

    feat: add key store for timerange

--- app/screens/DetailCrypto/stores/index.ts
+++ app/screens/DetailCrypto/stores/index.ts
@@ -3,17 +3,22 @@ import { create } from "zustand";
 type TCryptoDetail = {
   selectedSymbol: string;
   lastPrice: any;
+  timeRangeSelected: string;
   setSelectedSymbol: (symbol: string) => void;
   setLastPrice: (price: any) => void;
+  setTimeRangeSelected: (time: string) => void;
   onReset: () => void;
 };
 
 const useGetCryptoDetail = create<TCryptoDetail>((set) => ({
   selectedSymbol: "BTCUSDT",
   lastPrice: {},
+  timeRangeSelected: "1m",
+  setTimeRangeSelected: (time: string) => set({ timeRangeSelected: time }),
   setSelectedSymbol: (symbol: string) => set({ selectedSymbol: symbol }),
   setLastPrice: (price: any) => set({ lastPrice: price }),
-  onReset: () => set({ selectedSymbol: "BTCUSDT", lastPrice: {} }),
+  onReset: () =>
+    set({ selectedSymbol: "BTCUSDT", lastPrice: {}, timeRangeSelected: "1m" }),
 }));
 
 export default useGetCryptoDetail;
