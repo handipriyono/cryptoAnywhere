02933a84c9bcfea7740c149a9b0f2a52f2216bc7
Handi Priyono <handimacbook@gmail.com>
2024-02-28 09:47:56 +0700

    feat: add commons component header

---
+++ src/Commons/Components/HeaderBackDetail/index.tsx
@@ -0,0 +1,32 @@
+import { View, Text, TouchableOpacity } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import useGetCryptoDetail from "../../../screens/DetailCrypto/stores";
+import Styles from "./style";
+
+const HeaderBack = () => {
+  const { selectedSymbol } = useGetCryptoDetail();
+  return (
+    <View>
+      <View style={Styles.headerBackWrapper}>
+        <View>
+          <TouchableOpacity>
+            <Ionicons name="arrow-back" size={20} />
+          </TouchableOpacity>
+        </View>
+        <View>
+          <Text style={{ fontWeight: "bold" }}>
+            {selectedSymbol} Token Price
+          </Text>
+        </View>
+        <View>
+          <TouchableOpacity>
+            <AntDesign name={"star"} size={20} color="orange" />
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default HeaderBack;
---
+++ src/Commons/Components/HeaderBackDetail/style.tsx
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  headerBackWrapper: {
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    paddingTop: 15,
+    minHeight: 40,
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    borderWidth: 0,
+    borderBottomWidth: 1,
+    borderColor: "#eee",
+    backgroundColor: "#fff",
+    paddingBottom: 10,
+  },
+});
